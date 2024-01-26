3e5ff066d5263a73031f76db204151f7fa76f7ce
quangthanhphi <phithanh003@gmail.com>
2024-01-27 00:58:09 +0700

    First End Point

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
@@ -13,12 +14,15 @@ namespace MagicVilla_VillaAPI.Controllers
 		[HttpGet]
 		public IEnumerable<VillaDTO> GetVillas()
 		{
-			return new List<VillaDTO>
-			{
-				new VillaDTO {Id = 1, Name="Pool View" },
-				new VillaDTO {Id = 2, Name="Beach View" }
-			};
+			return VillaStore.villaList;
 		}
-	}
+
+        [HttpGet("id:int")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+        }
+
+    }
 }
 
